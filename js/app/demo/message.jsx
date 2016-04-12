9023b1db8486f50a3b04062ec84e50ceadd2930a
Ilya Volodin <ivolodin@gmail.com>
2016-04-11 23:34:48 -0400

    Parial parserOptions

--- js/app/demo/message.jsx
+++ js/app/demo/message.jsx
@@ -6,7 +6,7 @@ define(['react'], function(React) {
         render: function() {
             return (
                 <div className={"alert alert-" + (this.props.value.fatal || this.props.value.severity === 2 ? "danger" : "warning" )} title={this.props.value.message}>
-                    {this.props.value.line}:{this.props.value.column} - {this.props.value.message} (<a href="http://eslint.org/docs/rules/{this.props.value.ruleId}">{this.props.value.ruleId}</a>)
+                    {this.props.value.line}:{this.props.value.column} - {this.props.value.message} (<a href={"http://eslint.org/docs/rules/" + this.props.value.ruleId}>{this.props.value.ruleId}</a>)
                 </div>
             );
         }
