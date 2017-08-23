6b52d1438897d572bf268b811b0f67a52ac25eff
Teddy Katz <teddy.katz@gmail.com>
2017-08-23 14:01:38 -0400

    Avoid unnecessary empty parens in demo messages for fatal errors (#380)

--- js/app/demo/message.jsx
+++ js/app/demo/message.jsx
@@ -9,7 +9,14 @@ define(['react', 'events'], function(React, events) {
         render: function() {
             return (
                 <div className={"alert alert-" + (this.props.value.fatal || this.props.value.severity === 2 ? "danger" : "warning" )} title={this.props.value.message} onClick={this.handleClick}>
-                    {this.props.value.line}:{this.props.value.column} - {this.props.value.message} (<a href={"http://eslint.org/docs/rules/" + this.props.value.ruleId}>{this.props.value.ruleId}</a>)
+                    {this.props.value.line}:{this.props.value.column} - {this.props.value.message}
+                    {
+                        !this.props.value.fatal && [
+                            ' (',
+                            <a href={"https://eslint.org/docs/rules/" + this.props.value.ruleId}>{this.props.value.ruleId}</a>,
+                            ')'
+                        ]
+                    }
                 </div>
             );
         }
