9d0d202358710ba93e6c56ed5430da88e265c9ee
Jan Pilzer <jan.pilzer@gmx.de>
2018-11-13 11:52:13 -0800

    Run build for parser demo

--- js/app/parser/index.built.js
+++ js/app/parser/index.built.js
@@ -1,134 +1,150 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
 // requires
-var config = require('./parser.config');
-var lib = require('./lib');
+var config = require("./parser.config");
+var lib = require("./lib");
 
 // DOM stuff
-var editor = document.querySelector('#editor');
-var results = document.querySelector('#results');
-var controls = document.querySelector('#configuration');
+var editor = document.querySelector("#editor");
+var results = document.querySelector("#results");
 
-function handleSelectChange(property, el, typeFunc) {
-	var selected = typeFunc(el.value);
-	config[property] = selected;
+function handleSelectChange(property, el) {
+    var selected = el.value;
+
+    config[property] = selected;
 }
 
 function toggleOption(option, property) {
-	if (property) {
-		config[property][option] = !config[property][option];
-	} else {
-		config[option] = !config[option];
-	}
+    if (property) {
+        config[property][option] = !config[property][option];
+    } else {
+        config[option] = !config[option];
+    }
 }
 
-function drawCheckboxes(config, property) {
-	var el = document.querySelector('.' + (property || 'options'));
+function drawCheckboxes(boxesConfig, property) {
+    var el = document.querySelector("." + (property || "options"));
 
-	// convert the list of options into checkboxes
-	var checkboxes = Object.keys(config).filter(function(option) {
-		return typeof config[option] === 'boolean';
-	}).map(function(option) {
-		var checked = config[option] ? ' checked ' : ' ';
-		return '<div class="checkbox"><label><input' + checked + 'type="checkbox" class="option-checkbox" id="' + option + '" />' + option + '</label></div>';
-	});
+    // convert the list of options into checkboxes
+    var checkboxes = Object.keys(boxesConfig).filter(function(option) {
+        return typeof boxesConfig[option] === "boolean";
+    }).map(function(option) {
+        var checked = boxesConfig[option] ? " checked " : " ";
 
-	el.innerHTML = checkboxes.join('');
-	el.onchange = function(event) {
-		toggleOption(event.target.id, property);
-	}
+        return "<div class=\"checkbox\"><label><input" + checked + "type=\"checkbox\" class=\"option-checkbox\" id=\"" + option + "\" />" + option + "</label></div>";
+    });
+
+    el.innerHTML = checkboxes.join("");
+    el.onchange = function(event) {
+        toggleOption(event.target.id, property);
+    };
 
 }
 
-function drawSelectBoxes(config, property, typeFunc) {
-	var el = document.querySelector('.' + property);
-	var select = document.createElement('select');
-	var options = Object.keys(config).filter(function(option) {
-		return typeof config[option] === 'boolean';
-	}).map(function(option) {
-		var selected = config[option] ? ' selected ' : ' ';
-		return '<option ' + selected + ' value="' + option + '">' + option +'</option>';
-	});
-	select.innerHTML = options;
-	select.onchange = function(event) {
-		handleSelectChange(property, event.target, typeFunc);
-	};
-	el.appendChild(select);
+function drawSelectBoxes(boxesConfig, property) {
+    var el = document.querySelector("." + property);
+    var select = document.createElement("select");
+    var options = Object.keys(boxesConfig).filter(function(option) {
+        return typeof boxesConfig[option] === "boolean";
+    }).map(function(option) {
+        var selected = boxesConfig[option] ? " selected " : " ";
 
-	// reset the config to take the currently selected value
-	handleSelectChange(property, select, typeFunc);
+        return "<option " + selected + " value=\"" + option + "\">" + option + "</option>";
+    });
+
+    select.innerHTML = options;
+    select.onchange = function(event) {
+        handleSelectChange(property, event.target);
+    };
+    el.appendChild(select);
+
+    // reset the config to take the currently selected value
+    handleSelectChange(property, select);
 }
 
 function drawVersion() {
-	document.getElementById('version').innerHTML = lib.getVersion();
+    document.getElementById("version").innerHTML = lib.getVersion();
 }
 
 function setupControls() {
-	drawVersion();
-	drawCheckboxes(config);
-	drawCheckboxes(config.ecmaFeatures, "ecmaFeatures");
-	drawSelectBoxes(config.ecmaVersion, "ecmaVersion", Number);
-	drawSelectBoxes(config.sourceType, "sourceType", String);
+    drawVersion();
+    drawCheckboxes(config);
+    drawCheckboxes(config.ecmaFeatures, "ecmaFeatures");
+    drawSelectBoxes(config.ecmaVersion, "ecmaVersion");
+    drawSelectBoxes(config.sourceType, "sourceType");
 }
 
 function showError(error) {
-	console.log(error);
-	results.classList.add('error');
-	results.innerHTML = error.message;
-	console.warn(error.message);
+    console.error(error); // eslint-disable-line no-console
+    results.classList.add("error");
+    results.innerHTML = error.message;
 }
 
 function parseForever() {
-	var ast;
-	try {
-		ast = lib.parse(editor.value, config);
-		results.classList.remove('error');
-		results.innerHTML = ast;
-	} catch(error) {
-		showError(error);
-	}
-	setTimeout(parseForever, 1000)
+    var ast;
+
+    try {
+        ast = lib.parse(editor.value, config);
+        results.classList.remove("error");
+        results.innerHTML = ast;
+    } catch (error) {
+        showError(error);
+    }
+    setTimeout(parseForever, 1000);
 }
 
 // set everything up
 setupControls();
 parseForever();
+
 },{"./lib":2,"./parser.config":3}],2:[function(require,module,exports){
+/* eslint-env commonjs */
+
+"use strict";
+
 // requires
-var espree = require('espree');
+var espree = require("espree");
 
 exports.parse = function(code, config) {
-	var ast = espree.parse(code, config); // throws...
-	return JSON.stringify(ast, null, '  ');
+    var ast = espree.parse(code, config); // throws...
+
+    return JSON.stringify(ast, null, "  ");
 };
 
 exports.getVersion = function() {
-	return espree.version;
+    return espree.version;
 };
 
 },{"espree":"espree"}],3:[function(require,module,exports){
+/* eslint-env commonjs */
+
+"use strict";
+
 module.exports = {
-	range: false,
-	loc: false,
-	comments: false,
-	attachComment: false,
-	tokens: false,
-	ecmaVersion: {
-		3: false,
-		5: false,
-		6: true,
-		7: false,
-		8: false
-	},
-	sourceType: {
-		script: false,
-		module: true
-	},
-	ecmaFeatures: {
-		jsx: true,
-		globalReturn: true,
-		impliedStrict: false,
-		experimentalObjectRestSpread: true
-	}
+    range: false,
+    loc: false,
+    comments: false,
+    attachComment: false,
+    tokens: false,
+    ecmaVersion: {
+        3: false,
+        5: false,
+        6: true,
+        7: false,
+        8: false,
+        9: false
+    },
+    sourceType: {
+        script: false,
+        module: true
+    },
+    ecmaFeatures: {
+        jsx: true,
+        globalReturn: true,
+        impliedStrict: false,
+        experimentalObjectRestSpread: true
+    }
 };
 
 },{}]},{},[1]);
