aa76758724b0b95cca60f95300749585c49d48a6
Nicholas C. Zakas <nicholas@nczconsulting.com>
2019-02-08 10:32:12 -0800

    New: Incorporate sponsors into the site
    * Move "Who's using ESLint" to /users
    * Add "Who's using ESLint" to About menu
    * Add script to pull backers from Open Collective
    * Incorporate sponsors on the homepage

---
+++ _tools/fetch-sponsors.js
@@ -0,0 +1,72 @@
+/**
+ * @fileoverview Script to fetch sponsor data from Open Collective. Call using:
+ *     node _tools/fetch-sponsors.js
+ * @author Nicholas C. Zakas
+ */
+
+/* eslint camelcase: [error, { properties: never }] */
+
+"use strict";
+
+//-----------------------------------------------------------------------------
+// Requirements
+//-----------------------------------------------------------------------------
+
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+//-----------------------------------------------------------------------------
+// Data
+//-----------------------------------------------------------------------------
+
+// filename to output to
+const filename = "./_data/sponsors.json";
+
+// simplified data structure
+const sponsors = {
+    gold: [],
+    silver: [],
+    bronze: [],
+    backers: []
+};
+
+//-----------------------------------------------------------------------------
+// Main
+//-----------------------------------------------------------------------------
+
+(async() => {
+
+    // fetch the data
+    const result = await fetch("https://opencollective.com/api/groups/eslint/backers");
+    const backers = await result.json();
+
+    // process into a useful format
+    for (const backer of backers) {
+        const sponsor = {
+            name: backer.name,
+            url: backer.website,
+            image: backer.avatar,
+            totalDonations: backer.totalDonations
+        };
+
+        switch (backer.tier) {
+            case "gold-sponsor":
+                sponsors.gold.push(sponsor);
+                break;
+                
+            case "silver-sponsor":
+                sponsors.silver.push(sponsor);
+                break;
+
+            case "bronze-sponsor":
+                sponsors.bronze.push(sponsor);
+                break;
+
+            default:
+                sponsors.backers.push(sponsor);
+
+        }
+    }
+
+    fs.writeFileSync(filename, JSON.stringify(sponsors, null, "    "), { encoding: "utf8" });
+})();
