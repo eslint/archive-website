cbac103a5eb93c40848988dca0073c83412ece5b
Kai Cataldo <kaicataldo@gmail.com>
2019-07-09 23:29:11 +0000

    Use Netlify's env vars for prod

--- _plugins/config_url.rb
+++ _plugins/config_url.rb
@@ -1,10 +1,14 @@
-PRODUCTION_URL = 'https://eslint.org'
-DEPLOYMENT_PREVIEW_URL = ENV['DEPLOY_PRIME_URL']
+# Netlify sets the following environment variables
+# https://www.netlify.com/docs/continuous-deployment/#environment-variables
+DEPLOY_URL = (ENV['CONTEXT'].eql? 'production') ? ENV['URL'] : ENV['DEPLOY_PRIME_URL']
+
+# This is for backwards compatibility for the version currently deployed with GitHub Pages
+DEFAULT_URL = 'https://eslint.org'
 
 module Jekyll
     class EnvironmentVariablesGenerator < Generator
         def generate(site)
-            site.config['url'] = DEPLOYMENT_PREVIEW_URL || PRODUCTION_URL
+            site.config['url'] = DEPLOY_URL || DEFAULT_URL
         end
     end
 end
