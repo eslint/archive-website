fbc09f5aa673222cb0ede4c58ba984158df0ac99
Kai Cataldo <kai@kaicataldo.com>
2019-11-18 18:55:56 -0500

    Add blog posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,47 @@
 "use strict";
 
 const fs = require("fs");
+const moment = require("moment");
+const htmlparser2 = require("htmlparser2");
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addCollection("posts", collection => collection.getFilteredByGlob("_posts/*.md"));
+    eleventyConfig.addCollection("posts", collection => collection.getFilteredByGlob("_posts/*.md").reverse());
+
+    // https://www.11ty.io/docs/dates/#dates-off-by-one-day
+    eleventyConfig.addFilter("date", (date, format) => moment(date.toUTCString()).format(format));
+    eleventyConfig.addFilter("numberOfWords", templateContent => {
+        let htmlTextContent = "";
+        let isInCodeBlock = false;
+
+        const parser = new htmlparser2.Parser(
+            {
+                onopentag(tagName) {
+                    if (tagName !== "pre") {
+                        return;
+                    }
+                    isInCodeBlock = true;
+                },
+                ontext(text) {
+
+                    // Ignore code examples and filter out punctuation.
+                    if (isInCodeBlock || !/[\w\d]/.test(text)) {
+                        return;
+                    }
+                    htmlTextContent += ` ${text.trim()}`;
+                },
+                onclosetag(tagName) {
+                    if (tagName !== "pre") {
+                        return;
+                    }
+                    isInCodeBlock = false;
+                }
+            },
+            { decodeEntities: true }
+        );
+
+        parser.parseComplete(templateContent);
+        return htmlTextContent.trim().split(/\s+/).length;
+    });
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.setBrowserSyncConfig({
         callbacks: {
