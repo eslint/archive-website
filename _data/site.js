9b0279403c76e146e43c1ce652d2b6ed20f7589a
Kai Cataldo <kai@kaicataldo.com>
2019-11-18 20:11:01 -0500

    Fix index/404 pages

--- _data/site.js
+++ _data/site.js
@@ -1,15 +1,12 @@
 "use strict";
 
-const DEFAULT_URL = "https://eslint.org";
-
 module.exports = {
     title: "ESLint - Pluggable JavaScript linter",
-    repository: "eslint/eslint.github.io",
     description: "A pluggable and configurable linter tool for identifying and reporting on patterns in JavaScript. Maintain your code quality with ease.",
 
     /*
      * Netlify sets the following environment variables
      * https://www.netlify.com/docs/continuous-deployment/#environment-variables
      */
-    url: (process.env.CONTEXT === "production" ? process.env.URL : process.env.DEPLOY_PRIME_URL) || DEFAULT_URL
+    url: (process.env.CONTEXT === "production" ? process.env.URL : process.env.DEPLOY_PRIME_URL) || ""
 };
