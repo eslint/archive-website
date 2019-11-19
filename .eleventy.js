8c6f9ce528a13a2c3eaa7d97db4c17d110f2aa1c
Kai Cataldo <kai@kaicataldo.com>
2019-11-18 21:05:28 -0500

    Move filters into separate module

--- .eleventy.js
+++ .eleventy.js
@@ -1,54 +1,20 @@
 "use strict";
 
 const fs = require("fs");
-const moment = require("moment");
-const htmlparser2 = require("htmlparser2");
+const { addFilters } = require("./_11ty/filters");
 
 module.exports = function(eleventyConfig) {
+    addFilters(eleventyConfig);
 
-    // https://www.11ty.io/docs/dates/#dates-off-by-one-day
-    eleventyConfig.addFilter("date", (date, format) => moment(date.toUTCString()).format(format));
-    eleventyConfig.addFilter("numberOfWords", templateContent => {
-        let htmlTextContent = "";
-        let isInCodeBlock = false;
-
-        const parser = new htmlparser2.Parser(
-            {
-                onopentag(tagName) {
-                    if (tagName !== "pre") {
-                        return;
-                    }
-                    isInCodeBlock = true;
-                },
-                ontext(text) {
-
-                    // Ignore code examples and filter out punctuation.
-                    if (isInCodeBlock || !/[\w\d]/.test(text)) {
-                        return;
-                    }
-                    htmlTextContent += ` ${text.trim()}`;
-                },
-                onclosetag(tagName) {
-                    if (tagName !== "pre") {
-                        return;
-                    }
-                    isInCodeBlock = false;
-                }
-            },
-            { decodeEntities: true }
-        );
-
-        parser.parseComplete(templateContent);
-        return htmlTextContent.trim().split(/\s+/).length;
-    });
     eleventyConfig.addCollection("posts", collection => collection.getFilteredByGlob("_posts/*.md").reverse());
     eleventyConfig.addPassthroughCopy("assets");
+
+    // Handle 404s when developing locally.
     eleventyConfig.setBrowserSyncConfig({
         callbacks: {
             // eslint-disable-next-line handle-callback-err
             ready(err, browserSync) {
 
-                // Handle 404s when developing locally.
                 const page404 = fs.readFileSync("_site/404.html");
 
                 browserSync.addMiddleware("*", (req, res) => {
---
+++ _11ty/filters/index.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const moment = require("moment");
+const htmlparser2 = require("htmlparser2");
+
+const filters = {
+    dateFormat(date, format) {
+
+        /*
+        * Convert to UTCString to avoid off by one error.
+        * https://www.11ty.io/docs/dates/#dates-off-by-one-day
+        */
+        return moment(date.toUTCString()).format(format);
+    },
+    numberOfWords(templateContent) {
+        let htmlTextContent = "";
+        let isInCodeBlock = false;
+
+        const parser = new htmlparser2.Parser(
+            {
+                onopentag(tagName) {
+                    if (tagName !== "pre") {
+                        return;
+                    }
+                    isInCodeBlock = true;
+                },
+                ontext(text) {
+
+                    // Ignore code examples and filter out punctuation.
+                    if (isInCodeBlock || !/[\w\d]/.test(text)) {
+                        return;
+                    }
+                    htmlTextContent += ` ${text.trim()}`;
+                },
+                onclosetag(tagName) {
+                    if (tagName !== "pre") {
+                        return;
+                    }
+                    isInCodeBlock = false;
+                }
+            },
+            { decodeEntities: true }
+        );
+
+        parser.parseComplete(templateContent);
+        return htmlTextContent.trim().split(/\s+/).length;
+    }
+}
+
+module.exports = {
+    addFilters(eleventyConfig) {
+        for (const [name, filter] of Object.entries(filters)) {
+            eleventyConfig.addFilter(name, filter);
+        }
+    }
+}
