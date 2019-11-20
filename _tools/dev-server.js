5f8a1a70c2fbfb47be2074a6fdb06de0a3933416
Kai Cataldo <kai@kaicataldo.com>
2019-11-20 15:18:54 -0500

    Watch styles in local dev

--- _tools/dev-server.js
+++ _tools/dev-server.js
@@ -1,12 +1,20 @@
+/**
+ * @fileoverview Local development server that emulates our Netlify deployment environments.
+ *     Watches for changes and rebuilds source files as they are edited.
+ * @author Kai Cataldo
+ */
+
 /* eslint-disable no-console */
 
 "use strict";
 
 const path = require("path");
-const { spawn } = require("child_process");
+const { spawn, exec } = require("child_process");
 const express = require("express");
+const chokidar = require("chokidar");
 
 const PORT = 8080;
+const ROOT_DIR = path.resolve(__dirname, "..");
 const app = express();
 
 function formatStreamData(data) {
@@ -16,7 +24,7 @@ function formatStreamData(data) {
 function spawnChildProcess(command, args) {
     const processName = `${command} ${args.join(" ")}`;
     const options = {
-        cwd: path.resolve(__dirname, ".."),
+        cwd: ROOT_DIR,
         shell: true
     };
     const childProcess = spawn(command, args, options);
@@ -34,6 +42,15 @@ function spawnChildProcess(command, args) {
 app.use(express.static(path.resolve(__dirname, "../_site"), { extensions: ["html"] }));
 app.use((req, res) => res.status(404).sendFile(path.resolve(__dirname, "../_site/404.html")));
 app.listen(PORT, () => {
+
+    // Less dosen't have a maintained watcher.
+    chokidar.watch(path.resolve(ROOT_DIR, "src/styles/**/*.less"))
+        .on("change", filePath => {
+            console.log(`\n${filePath} changed. Recompiling styles.`);
+            spawnChildProcess("npm", ["run", "build:less"]);
+        })
+        .on("ready", () => spawnChildProcess("npm", ["run", "build:less"]));
+
     spawnChildProcess("npm", ["run", "start:eleventy"]);
     spawnChildProcess("npm", ["run", "start:webpack"]);
     console.log(`Local version of eslint.org can be accessed at http://localhost:${PORT}\n`);
