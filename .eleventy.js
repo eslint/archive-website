bf00acbf05c1ade23209a7332b6c57fb78903ec5
Kai Cataldo <kai@kaicataldo.com>
2020-01-08 18:42:22 -0500

    Use built-in data source API for YAML

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 "use strict";
 
 const { addPlugins, addFilters, addTransforms } = require("./_11ty");
+const yaml = require("js-yaml");
 
 module.exports = function(eleventyConfig) {
     addPlugins(eleventyConfig);
     addFilters(eleventyConfig);
     addTransforms(eleventyConfig);
+
+    // Add YAML files as data sources
+    eleventyConfig.addDataExtension("yml", contents => yaml.safeLoad(contents));
 
     // Add collection of blog posts.
     eleventyConfig.addCollection("posts", collection => collection.getFilteredByGlob("_posts/*.md").reverse());
